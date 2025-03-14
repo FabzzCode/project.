62b476d23ada608037f1948ec36cd05ed5fd6799
FabzzCode <fabbxml@gmail.com>
2025-03-14 09:04:10 +0700

    Update florese-circle.py

--- florese-circle.py
+++ florese-circle.py
@@ -4,16 +4,16 @@ t = turtle.Turtle()
 s = turtle.Screen()
 
 s.bgcolor('#262626')
-t.pencolor('magenta')
+t.pencolor('black')
 t.speed(100)
 
-col = ('cyan', 'yellow', 'red', 'light green')
+col = ('red', 'yellow', 'orange', 'green')
 
 for n in range(10):
     for x in range(8):
         t.speed(x+10)
         for i in range(2):
-            t.pensize(2)
+            t.pensize(1)
             t.circle(80+n*20, 90)
             t.lt(90)
         t.lt(45)
